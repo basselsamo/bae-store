586f842871c07703268ab2ed51fe777142921740
basselsamo <basselsamo@hotmail.com>
2024-05-03 13:44:07 +0200

    enhanced routes to serve external files

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 "use strict"
-const routeResponseMap = {
-    "/home": "<h1>Home Page</h1>",
-    "/women": "<h1>Women Mode</h1>",
-    "/men": "<h1>Men Mode</h1>",
-    "/kids": "<h1>Kids Mode</h1>",
-    "/support": "<h1>Support & Contact</h1>"
-};
-
 const port = 3000;
 const http = require("http");
 const httpStatus = require("http-status-codes");
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {
+const fs = require("fs");
+const routeMap = {
+  "/": "views/index.html"
+};
+http.createServer((req, res) => {
+    res.writeHead(httpStatus.OK, {
       "Content-Type": "text/html"
 });
 
-if (routeResponseMap[req.url]) {
-    res.end(routeResponseMap[req.url]);
-  } else {
-    res.end("<h1>Welcome to BAE Store!</h1>");
+if (routeMap[req.url]) {
+  fs.readFile(routeMap[req.url], (error, data) => {
+  res.write(data);
+  res.end();
+  }); 
+} else {
+    res.end("<h1>Error 404! Not Found</h1>");
   }
-});
-app.listen(port);
+})
+.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
